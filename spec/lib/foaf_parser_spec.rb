d8e94a0f286885d74845602527fbd39f3972ea7d
Laura Wilsey <lauraw15@stanford.edu>
2016-07-26 15:46:35 -0700

    adding method 'introduces itself'

--- spec/lib/foaf_parser_spec.rb
+++ spec/lib/foaf_parser_spec.rb
@@ -6,4 +6,10 @@ describe FoafParser do
     fp = FoafParser.new()
     expect(fp).to be_kind_of(FoafParser)
   end
+  
+  it 'introduces itself' do
+    fp = FoafParser.new()
+    expect(fp.speak).to eq("I'm FoafParser!")
+  end
+  
 end
