8396ee9ab6556f1153bbd3d992437dbad351405e
Laura Wilsey <lauraw15@stanford.edu>
2016-08-02 14:47:45 -0700

    adding URL instance variable

--- lib/foaf_parser.rb
+++ lib/foaf_parser.rb
@@ -1,6 +1,11 @@
 class FoafParser
+
+  attr_accessor :url
+
   def initialize(rdf_url="http://stanford.edu/~sdoljack/sdoljack_foaf.rdf")
+    @url=rdf_url
   end
+  
   def speak
     "I'm FoafParser!"
   end
--- spec/lib/foaf_parser_spec.rb
+++ spec/lib/foaf_parser_spec.rb
@@ -5,6 +5,7 @@ describe FoafParser do
   it 'instantiates' do
     fp = FoafParser.new()
     expect(fp).to be_kind_of(FoafParser)
+    expect(fp.url).to eq("http://stanford.edu/~sdoljack/sdoljack_foaf.rdf")
   end
   
   it 'introduces itself' do
@@ -15,6 +16,11 @@ describe FoafParser do
   it 'instantiates with rdf' do
     fp = FoafParser.new("http://stanford.edu/~bess/foaf.rdf")
     expect(fp).to be_kind_of(FoafParser)
+  end
+  
+  it 'has URL' do
+    fp = FoafParser.new("http://stanford.edu/~bess/foaf.rdf")
+    expect(fp.url).to eq("http://stanford.edu/~bess/foaf.rdf")
   end
 
   
